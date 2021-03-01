cc155dd66afd9b7932c896222c2aa1ae5c401056
TravisSLee <travisslee223@gmail.com>
2021-03-01 12:44:14 -0500

    commit tree 94ffa2b608d7d65ed7b951b5046238767ac87f06

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 2;
 
-// Write your code here
+let num2 = 31;
+
+let multiply = num1 * num2;
+
+let random = Math.round(Math.random() * 10);
+
+let num3 = 11
+
+let num4 = 7;
+let mod = num3 % num4;
+
+let set = [20, 2, 5];
+let max = Math.max(20, 2, 5);
